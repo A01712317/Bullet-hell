06fe0971fb9056966b34da0860a0c28f842de099
A01712317 <A01712317@tec.mx>
2025-11-19 00:11:26 -0600

    la camara y el jugador no se salen del tablero

--- My project (1)/Assets/Scripts/MisScripts/CameraPlayer.cs
+++ My project (1)/Assets/Scripts/MisScripts/CameraPlayer.cs
@@ -1,37 +1,29 @@
 ﻿using UnityEngine;
 
-
-/// <summary>
-/// Este controlador ejecutar� el movimiento de la c�mara del jugador
-/// est�ndar de codificaci�n:
-/// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
-/// </summary>
 public class CameraPlayer : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 0, -10);
-    /// <summary>
-    /// Start is called once before the first execution of Update after the MonoBehaviour is created
-    /// </summary>
+    public Transform player;
+    public Vector2 minBounds = new Vector2(-8, -5);
+    public Vector2 maxBounds = new Vector2(8, 5);
+
+    private float halfHeight;
+    private float halfWidth;
+
     void Start()
     {
-
-    }
-
-    /// <summary>
-    /// Update is called once per frame
-    /// </summary>
-    void Update()
-    {
-        transform.position = player.transform.position + offset;
-
+        Camera cam = GetComponent<Camera>();
+        halfHeight = cam.orthographicSize;
+        halfWidth = halfHeight * cam.aspect;
     }
 
     void LateUpdate()
     {
-        Vector3 pos = transform.position;
-        pos.z = 0;
-        transform.position = pos;
+        Vector3 targetPos = player.position;
+
+        float clampedX = Mathf.Clamp(targetPos.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(targetPos.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 }
 
---
+++ My project (1)/Assets/Scripts/MisScripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Este controllador actualizar� los eventos del veh�culo del jugador
+/// Estandar de codificaci�n: 
+/// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/documentation-comments
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    //Variables c�mara
+    public Camera mainCamera;
+
+    public float speed = 5.0f;
+    public float turnSpeed = 0.0f;
+    public float horizontalInput;
+    public float forwardInput;
+
+    public Vector2 minBounds = new Vector2(-8, -5);
+    public Vector2 maxBounds = new Vector2(8, 5);
+    /// <summary>
+    /// Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// </summary>
+    /// 
+    void LateUpdate()
+    {
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x, maxBounds.x);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y, maxBounds.y);
+
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.up * Time.deltaTime * speed * forwardInput);
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * turnSpeed);
+        
+    }
+}
